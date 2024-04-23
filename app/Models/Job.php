2b8647824b8d8047ca38e0d83d97c888640662c7
Gene Kelley <phpcodehead@gmail.com>
2024-04-23 17:06:01 -0500

    Add Job model and reference in routes.

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,41 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id'     => 1,
-                'title'  => 'Director',
-                'salary' => '$50,000',
-                'pay_frequency' => 'year',
-            ],
-            [
-                'id'     => 2,
-                'title'  => 'Programmer',
-                'salary' => '$10,000',
-                'pay_frequency' => 'month',
-            ],
-            [
-                'id'     => 3,
-                'title'  => 'Teacher',
-                'salary' => '$40,000',
-                'pay_frequency' => 'year',
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404, 'Job not found');
-        }
-
-        return $job;
-    }
+class Job extends Model {
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary', 'pay_frequency'];
 }
