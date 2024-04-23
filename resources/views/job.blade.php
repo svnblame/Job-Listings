b7044af383937e3fb8e28c67172e93707cf11dc8
Gene Kelley <phpcodehead@gmail.com>
2024-04-23 15:49:10 -0500

    Add initial migration scripts and change column name 'pay-frequency' to 'pay_frequency'.

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -4,6 +4,6 @@
     </x-slot:heading>
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
     <p>
-        This job pays {{ $job['salary'] }} per {{ $job['pay-frequency'] }}.
+        This job pays {{ $job['salary'] }} per {{ $job['pay_frequency'] }}.
     </p>
 </x-layout>
