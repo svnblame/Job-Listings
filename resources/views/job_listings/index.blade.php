25ddce0774f869e95675947cf08286f416816181
Gene Kelley <phpcodehead@gmail.com>
2024-05-03 17:03:12 -0700

    Add vite build npm package and localized Tailwind CSS.

--- resources/views/job_listings/index.blade.php
+++ resources/views/job_listings/index.blade.php
@@ -6,11 +6,11 @@
     <div class="space-y-4">
         @foreach ($jobs as $job)
             <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-200 rounded-lg hover:bg-sky-200 hover:text-blue-600">
-                <div class="font-bold text-blue-500 text-sm">
+                <div class="font-bold text-kelley text-sm">
                     {{ $job->employer->name }}
                 </div>
                 <div>
-                    <strong>{{ $job['title'] }}:</strong> Pays {{ $job->salaryUS() }} per {{ $job['pay_frequency'] }}
+                    <strong class="text-kelley">{{ $job['title'] }}:</strong> Pays {{ $job->salaryUS() }} per {{ $job['pay_frequency'] }}
                 </div>
             </a>
         @endforeach
